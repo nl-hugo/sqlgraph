9d9ae1841ed8f41c3f71de79e178313280c221f3
nl-hugo <info@hugojanssen.nl>
2015-07-20 17:12:23 +0200

    More documentation

--- src/main/java/nl/hugojanssen/sqlgraph/model/sql/SQLWorkflow.java
+++ src/main/java/nl/hugojanssen/sqlgraph/model/sql/SQLWorkflow.java
@@ -11,6 +11,13 @@ import nl.hugojanssen.sqlgraph.visitors.SQLVisitor;
 
 import org.apache.log4j.Logger;
 
+/**
+ * A <code>SQLWorkflow</code> object contains the directories and files that are to be parsed. All files are accepted,
+ * but only SQL files will be parsed. The parse results are communicated through the <code>SQLVisitor</code> objects
+ * that can be registered with this workflow.
+ * 
+ * @author hjanssen
+ */
 public class SQLWorkflow
 {
 	/** The logger */
@@ -22,16 +29,44 @@ public class SQLWorkflow
 
 	private List<SQLVisitor> visitors = new ArrayList<SQLVisitor>();
 
+	/**
+	 * Construct a new <code>SQLWorkflow</code> object with a dummy name.
+	 */
 	public SQLWorkflow()
 	{
 		this( "dummy" );
 	}
 
+	/**
+	 * Construct a new <code>SQLWorkflow</code> object with the given name.
+	 * 
+	 * @param aName the workflow name
+	 */
 	public SQLWorkflow( String aName )
 	{
 		this.name = aName;
 	}
 
+	/**
+	 * Add a visitor to the list of visitors.
+	 * 
+	 * @param aVisitor the visitor to add
+	 */
+	public void addVisitor( SQLVisitor aVisitor )
+	{
+		if ( aVisitor != null )
+		{
+			this.visitors.add( aVisitor );
+		}
+	}
+
+	/**
+	 * Add a file to parse to the workflow. All files are accepted, but only SQL files are parsed. Other files are
+	 * simply ignored.
+	 * 
+	 * @param aFile a file or directory
+	 * @throws IOException when the file does not exist or is not readable.
+	 */
 	public void addWorkflowFile( File aFile ) throws IOException
 	{
 		LOG.debug( "addWorkflowFile " + aFile );
@@ -45,16 +80,42 @@ public class SQLWorkflow
 		}
 	}
 
+	/**
+	 * Returns the name of the workflow.
+	 * 
+	 * @return the name of the workflow
+	 */
 	public String getName()
 	{
 		return this.name;
 	}
 
+	/**
+	 * Returns all visitors.
+	 * 
+	 * @return the list of visitors
+	 */
+	public List<SQLVisitor> getVisitors()
+	{
+		return this.visitors;
+	}
+
+	/**
+	 * Returns the list of files to parse.
+	 * 
+	 * @return the list of files to parse
+	 */
 	public List<File> getWorkflowFiles()
 	{
 		return this.workflowFiles;
 	}
 
+	/**
+	 * Parses all files in the list of workflow files.
+	 * 
+	 * @return the number of errors that were encountered during parsing
+	 * @throws IOException when a file was not readable
+	 */
 	public int parse() throws IOException
 	{
 		int result = 0;
@@ -63,6 +124,22 @@ public class SQLWorkflow
 			result += this.parse( file );
 		}
 		return result;
+	}
+
+	/**
+	 * Removes all visitors.
+	 */
+	public void removeAllVisitors()
+	{
+		this.visitors.clear();
+	}
+
+	/**
+	 * Removes all files.
+	 */
+	public void removeAllWorkflowFiles()
+	{
+		this.workflowFiles.clear();
 	}
 
 	private int parse( final File aFile ) throws IOException
@@ -83,7 +160,7 @@ public class SQLWorkflow
 		{
 			LOG.debug( "Skipped file " + aFile.getName() + ": Not an SQL file" );
 		}
-		// signal completion
+		// TODO: signal completion
 		//this.parent.setComplete();
 
 		return result;
@@ -103,23 +180,5 @@ public class SQLWorkflow
 				LOG.warn( "Skipped file " + e.getMessage() );
 			}
 		}
-	}
-
-	public void addVisitor( SQLVisitor aVisitor )
-	{
-		if ( aVisitor != null )
-		{
-			this.visitors.add( aVisitor );
-		}
-	}
-
-	public List<SQLVisitor> getVisitors()
-	{
-		return this.visitors;
-	}
-
-	public void removeAllVisitors()
-	{
-		this.visitors.clear();
 	}
 }
